54a5078c88e610b7592249743218080b868d63a0
Jack Williams <jw@jackw.io>
2016-11-05 09:48:58 +0000

    Change to flx matching

--- .emacs.d/lisp/jw-modes.el
+++ .emacs.d/lisp/jw-modes.el
@@ -31,28 +31,33 @@
 
 ;; Ivy Mode
 (use-package ivy
- :config
- (progn
-   (ivy-mode 1)
-   (setq ivy-use-virtual-buffers t)
-   (setq ivy-use-virtual-buffers t)
-   (setq ivy-re-builders-alist '((t . ivy--regex-plus)))
-   (global-set-key "\C-s" 'swiper)
-   (global-set-key (kbd "C-c C-r") 'ivy-resume)
-   (global-set-key (kbd "C-x C-m") 'counsel-M-x)
-   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-   (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-   (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-   (global-set-key (kbd "<f1> l") 'counsel-load-library)
-   (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-   (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-   (global-set-key (kbd "C-c g") 'counsel-git)
-   (global-set-key (kbd "C-c j") 'counsel-git-grep)
-   (global-set-key (kbd "C-c k") 'counsel-ag)
-   (global-set-key (kbd "C-x l") 'counsel-locate)
-   (global-set-key (kbd "C-c w") 'ivy-push-view)
-   (global-set-key (kbd "C-c W") 'ivy-pop-view)
-   (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-   ))
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-re-builders-alist
+          '((ivy-switch-buffer . ivy--regex-plus)
+            (t . ivy--regex-fuzzy)))
+    (setq ivy-initial-inputs-alist nil)
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    (global-set-key (kbd "C-x C-m") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+    (global-set-key (kbd "<f1> l") 'counsel-load-library)
+    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+    (global-set-key (kbd "C-c g") 'counsel-git)
+    (global-set-key (kbd "C-c j") 'counsel-git-grep)
+    (global-set-key (kbd "C-c k") 'counsel-ag)
+    (global-set-key (kbd "C-x l") 'counsel-locate)
+    (global-set-key (kbd "C-c w") 'ivy-push-view)
+    (global-set-key (kbd "C-c W") 'ivy-pop-view)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+    ))
+
+
 
 (provide 'jw-modes)
