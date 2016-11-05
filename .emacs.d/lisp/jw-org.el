ff67c7f12ec78237d59aa052df4aa92426a60331
Jack Williams <jw@jackw.io>
2016-11-05 09:38:50 +0000

    Add new org captures

--- .emacs.d/lisp/jw-org.el
+++ .emacs.d/lisp/jw-org.el
@@ -3,17 +3,24 @@
 ;; Org Settings
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'org)
 
-(defvar jw-org-dir "~/org")
+(defvar jw-org-dir "~/org/")
+(defvar jw-org-diary-file (concat jw-org-dir "diary.org"))
 
 (customize-set-value 'org-directory jw-org-dir)
+(customize-set-value 'org-agenda-diary-file jw-org-diary-file)
 (customize-set-value 'org-agenda-files `(,jw-org-dir))
-(customize-set-value 'calendar-mark-diary-entries-flag t)
+
 (setq org-agenda-file-regexp "\\`[^.].*\\.org\\'\\|[0-9]+")
 (setq org-capture-templates
       '(("t" "TODO" entry (file+headline "~/org/gtd.org" "Tasks")
-         "* TODO %?\n  %i\n")))
-(org-toggle-sticky-agenda 1)
+         "* TODO %?\n  %i\n")
+        ("d" "Diary - Date" entry (file+datetree+prompt jw-org-diary-file)
+         "* %?\n  %i %t\n" :kill-buffer t)
+        ("e" "Diary - Event" entry (file+datetree+prompt jw-org-diary-file)
+         "* %?\n  %i %T\n" :kill-buffer t)
+        ))
 
 (define-key global-map "\C-cc" 'org-capture)
 (define-key global-map "\C-cl" 'org-store-link)
