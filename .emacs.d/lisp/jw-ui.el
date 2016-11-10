4572a427427de6b5cb3f0d0529cd136b86135d2a
Jack Williams <jw@jackw.io>
2016-11-10 22:03:00 +0000

    Remove background mode for solarized

--- .emacs.d/lisp/jw-ui.el
+++ .emacs.d/lisp/jw-ui.el
@@ -24,9 +24,6 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/site-lisp/emacs-color-theme-solarized")
 (load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'light)
-(set-terminal-parameter nil 'background-mode 'light)
-
 
 
 (provide 'jw-ui)
