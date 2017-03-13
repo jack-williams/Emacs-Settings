2fe43f3e0462867db24cbd12a84f9d607f800f38
Jack Williams <jw@jackw.io>
2017-03-13 18:24:29 +0000

    Add fsharp mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (cider ace-window haskell-mode org-bullets avy use-package auctex powerline typescript-mode flycheck magit ivy counsel))))
+    (fsharp-mode cider ace-window haskell-mode org-bullets avy use-package auctex powerline typescript-mode flycheck magit ivy counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
