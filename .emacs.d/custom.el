f419575fa79a42116f3345cfceebab2567589e7c
Jack Williams <jw@jackw.io>
2017-10-02 01:16:33 +0100

    Update custom.el with new packages

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,7 +8,7 @@
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (spaceline spacemacs-theme tide smartparens solarized-theme fsharp-mode cider ace-window haskell-mode org-bullets avy use-package auctex powerline typescript-mode flycheck magit ivy counsel))))
+    (hydra yasnippet spaceline spacemacs-theme tide smartparens solarized-theme fsharp-mode cider ace-window haskell-mode org-bullets avy use-package auctex powerline typescript-mode flycheck magit ivy counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
