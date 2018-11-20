b08339acfe4d7e3fe77164153551d3b991959c6f
Jack Williams <jw@jackw.io>
2018-11-20 23:15:36 +0000

    Switch to doom line

--- .emacs.d/lisp/jw-ui.el
+++ .emacs.d/lisp/jw-ui.el
@@ -20,12 +20,22 @@
   (setq spacemacs-theme-org-agenda-height nil)
   (setq spacemacs-theme-org-height nil))
 
-(use-package spaceline
-  :demand t
-  :config
-  (require 'spaceline-config)
-  (setq powerline-default-separator 'utf-8)
-  (spaceline-spacemacs-theme))
+(use-package doom-modeline
+  :ensure t
+  :defer t
+  :hook
+  (after-init . (lambda ()
+                  (setq doom-modeline-height 20)
+                  (setq doom-modeline-bar-width 4)
+                  (setq doom-modeline-icon (not (eq system-type 'windows-nt)))
+                  (doom-modeline-init))))
+
+;; (use-package spaceline
+;;   :demand t
+;;   :config
+;;   (require 'spaceline-config)
+;;   (setq powerline-default-separator 'utf-8)
+;;   (spaceline-spacemacs-theme))
 
 ;; Make ace-window leading character face match buffer
 (custom-set-faces
